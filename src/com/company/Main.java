c5762e04675c8890150aeeb4b3e495c896078f66
KrystianCyga <259247@student.pwr.edu.pl>
2022-01-06 13:07:27 +0100

    Euklidesowa gra

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,14 +10,23 @@ public class Main {
         Scanner scan=new Scanner(System.in);
         m=scan.nextInt();
 
-        while(m>0){
-            a=scan.nextInt();
-            b=scan.nextInt();
+        while(m>0) {
+            a = scan.nextInt();
+            b = scan.nextInt();
+
+            while (a != b) {
+                if (a > b) {
+                    a = a - b;
+                } else if (b > a) {
+                    b = b - a;
+                }
+            }
+                if (a == b) {
+                    System.out.println(a + b);
+            }
 
             m--;
         }
-
-        System.out.println("eloelo");
 
     }
 }
