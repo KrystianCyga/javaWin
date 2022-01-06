33663480d1f89efae13a545d59b44ee4744b0d18
KrystianCyga <259247@student.pwr.edu.pl>
2022-01-06 12:51:50 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int m=0,a=0,b=0;
+
+        Scanner scan=new Scanner(System.in);
+        m=scan.nextInt();
+
+        while(m>0){
+            a=scan.nextInt();
+            b=scan.nextInt();
+
+            m--;
+        }
+
+        System.out.println("eloelo");
+
+    }
+}
